b23504731804eeabe92ad1b349a6b8bfd06021b8
assareh <assareh@hashicorp.com>
2020-02-19 20:46:18 -0800

    going back

--- remote_backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "remote" {
-    hostname     = "app.terraform.io"
-    organization = "multicloud-provisioning-demo"
-    workspaces {
-      name = "gcp-compute-instance-dev"
-    }
-  }
-}
