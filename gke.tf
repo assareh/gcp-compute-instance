6ff05793ee443b8c56129a0daa36ff8246971baa
assareh <assareh@hashicorp.com>
2020-02-19 20:10:05 -0800

    changing to separate node pool

--- gke.tf
+++ gke.tf
@@ -1,5 +1,32 @@
-resource "google_container_cluster" "primary" {
-  name               = "marcellus-wallace"
-  location           = var.gcp_region
-  initial_node_count = 3
+resource "google_container_cluster" "gke_primary_cluster" {
+  name     = "my-gke-cluster"
+  location = var.gcp_region
+
+  # We can't create a cluster with no node pool defined, but we want to only use
+  # separately managed node pools. So we create the smallest possible default
+  # node pool and immediately delete it.
+  remove_default_node_pool = true
+  initial_node_count       = 1
+
+}
+
+resource "google_container_node_pool" "gke_primary_preemptible_nodes" {
+  name       = "my-node-pool"
+  location   = var.gcp_region
+  cluster    = google_container_cluster.gke_primary_cluster.name
+  node_count = 1
+
+  node_config {
+    preemptible  = true
+    machine_type = "f1.micro"
+
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
+
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+  }
 }
